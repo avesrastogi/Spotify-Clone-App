9a4f6917efd6a06ad462de2cb6562c19fbc3b742
Avni <avesrastogi@gmail.com>
2020-10-11 02:42:34 +0530

    added login.js

--- spotify-clone-app/src/App.js
+++ spotify-clone-app/src/App.js
@@ -3,8 +3,14 @@ import './App.css';
 
 function App() {
   return (
+    //BEM
     <div className="app">
       <h1>Hey! Aves here, building a Spotify react app</h1>
+
+      {/* Spotify Logo*/}
+      {/* Login with spotify button */}
+      <Login />
+    
     </div>
   );
 }
---
+++ spotify-clone-app/src/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Login(props) {
+    return (
+        <div>
+            {/* Spotify Logo*/}
+            {/* Login with spotify button */}   
+        </div>
+    );
+}
+
+export default Login;
