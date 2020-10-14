895e525ce28c83078c2bd5a797be34ef721f4d96
Avni <avesrastogi@gmail.com>
2020-10-15 05:23:15 +0530

    added Player, Body, Sidebar

--- spotify-clone-app/src/App.js
+++ spotify-clone-app/src/App.js
@@ -1,13 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import Login from './Login';
 import { getTokenFromUrl } from "./spotify";
 import SpotifyWebApi from "spotify-web-api-js";
+import Player from "./Player";
+import { useDataLayerValue } from "./DataLayer";
 
 const spotify = new SpotifyWebApi(); 
 
 function App() {
-  const [token, setToken] = useState(null);
+  const [{ user, token }, dispatch] = useDataLayerValue();
 
   // Run code based on a given condition
   //will load when name component loads and changes
@@ -19,22 +21,29 @@ function App() {
     const _token = hash.access_token;
 
     if(_token) {
-      setToken(_token);
+      dispatch({
+        type: "SET_TOKEN",
+        token: _token,
+      });
 
       spotify.setAccessToken(_token);
 
-      spotify.getMe()
+      spotify.getMe().then((user) => {
+        dispatch({
+          type: "SET_USER",
+          user,
+        });
+      });
     }
-
-    console.log("Here is my token ", token);
   }, []);
 
+  
   return (
     //BEM
     <div className="app">
       {
         token ? (
-          <h1> I am logged in. </h1>
+          <Player />
           ) : (
           <Login />    
         )
