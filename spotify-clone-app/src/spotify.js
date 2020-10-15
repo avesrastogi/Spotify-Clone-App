2c6e00d480ce0d6156bc4cf8cc1867fb802b45a9
Avni <avesrastogi@gmail.com>
2020-10-15 09:57:31 +0530

    debugged each file

--- spotify-clone-app/src/spotify.js
+++ spotify-clone-app/src/spotify.js
@@ -19,20 +19,20 @@ const scopes = [
     "user-modify-playback-state",
 ];
 
-export const getTokenFromUrl = () => {
-    return window.location.hash
-      .substring(1)
-      .split("&")
-      .reduce((initial, item) => {
-        var parts = item.split("=");
-        initial[parts[0]] = decodeURIComponent(parts[1]);
-  
-        return initial;
-      }, {});
-  };
+export const getTokenFromResponse = () => {
+  return window.location.hash
+    .substring(1)
+    .split("&")
+    .reduce((initial, item) => {
+      var parts = item.split("=");
+      initial[parts[0]] = decodeURIComponent(parts[1]);
+
+      return initial;
+    }, {});
+};
 
 // generate web address to send user to that address
 // %20 will add ASCII space of 20%
-export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
-    "%20"
-  )}&response_type=token&show_dialog=true`;
+export const accessUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+  "%20"
+)}&response_type=token&show_dialog=true`;
